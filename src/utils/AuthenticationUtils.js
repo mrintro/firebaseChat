c032fd3f62732d6d215a777dc9cdeb99ed66092d
mrintro <aniketpanwar.1998@gmail.com>
2023-06-15 17:51:55 +0530

    Login screen init.

---
+++ src/utils/AuthenticationUtils.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const validateEmail = (email) => {
+    // validation logic.
+    return true;
+}
+
+const validatePassword = (password) => {
+    return true;
+}
+
+export {
+    validateEmail,
+    validatePassword
+}
